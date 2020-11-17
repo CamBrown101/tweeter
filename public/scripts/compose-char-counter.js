e9d4a6ee3824ce07f21d462a223163510074686f
cambrown101 <cameron-brown@live.com>
2020-11-17 12:19:24 -0800

    Fixed counter not changing back to black

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,10 +1,14 @@
 $(document).ready(() => {
   $('#tweet-text').on('input', function() {
-    let inputValue = this.value;
+    let inputValue = $(this).val();
     let counter = 140 - inputValue.length;
     $(this.form[2]).html(counter)
+
+    if (counter > 0) {
+      $(this.form[2]).removeClass('red-text')
+    }
     if (counter <= 0) {
-      $(this.form[2]).addClass('red-font')
+      $(this.form[2]).addClass('red-text')
     }
   })
 })
