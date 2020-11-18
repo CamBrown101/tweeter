4551fe1cce46b1f16ab666ab8baf706c744b42b8
cambrown101 <cameron-brown@live.com>
2020-11-17 19:59:36 -0800

    Add AJAX GET request to fetch tweets from /tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,32 +1,3 @@
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-
-
 $(document).ready(() => {
   const createTweetElement = (tweetObject) => {
     const datePosted = tweetObject.created_at
@@ -65,16 +36,22 @@ $(document).ready(() => {
       $('#tweet-container').append(createTweetElement(element))
     })
   }
-  renderTweets(data);
+  // renderTweets(data);
 
   const loadTweets = () => {
-    $.get('/tweets')
-  }
+    $.ajax('/tweets', { method: 'GET' })
+      .then((tweets) => {
+        renderTweets(tweets)
+      })
+  };
 
   $("#new-tweet-form").submit(function(event) {
     event.preventDefault();
     $.post('/tweets',
       $('#new-tweet-form').serialize());
+    this.reset();
+    loadTweets();
   })
+
 });
 
