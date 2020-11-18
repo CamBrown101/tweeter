b4e2a550394760fce2068bd0101e461d9044074d
cambrown101 <cameron-brown@live.com>
2020-11-17 20:48:29 -0800

    Refactor code

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
   $('#tweet-text').on('input', function() {
     let inputValue = $(this).val();
     let counter = 140 - inputValue.length;
-    $(this.form[2]).html(counter)
+    $(this.form[2]).text(140 - inputValue.length)
 
     if (counter >= 0) {
       $(this.form[2]).removeClass('red-text')
