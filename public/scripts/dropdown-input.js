7a0313f34cb79ab4a60266535e3a1725b2d78549
cambrown101 <cameron-brown@live.com>
2020-11-18 21:13:19 -0800

    Add some spacing

--- public/scripts/dropdown-input.js
+++ public/scripts/dropdown-input.js
@@ -6,10 +6,13 @@ $(document).ready(() => {
   $tweetBtn.mouseenter(() => {
     $arrows.addClass('animate__bounce animate__animated animate__infinite	infinite');
   });
+
   $tweetBtn.mouseleave(() => {
     $arrows.removeClass('animate__bounce animate__animated animate__infinite	infinite');
   });
+
   $tweetBtn.on('click', () => {
     $animatedTextarea.slideToggle('slow');
   })
+
 });
